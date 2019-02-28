4b1cf5f99a1cf5afd64b40935dbc39d62ae0d681
Ryan Pethel <arpethel@gmail.com>
2019-02-28 20:23:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ def word_substituter(tweet)
   tweet_arr.each do |word|
     dictionary_keys = dictionary.keys
     if dictionary_keys.include?(word)
-      return keys
+      return dictionary_keys
       # tweet_arr.replace.values_at(word)
 
     end
