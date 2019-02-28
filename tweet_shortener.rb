6100844ef3e8c6803d7cd92e10d0f13da5a49d1b
Ryan Pethel <arpethel@gmail.com>
2019-02-28 21:16:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -46,6 +46,6 @@ def shortened_tweet_truncator(tweet)
   tweet_length = selective_tweet_shortener(tweet).length
   if tweet_length > 140
     # puts string characters up to 136 characters, and concats with ...
-    return tweet[0..137].concat("...")
+    puts tweet[0..137].concat("...")
   end
 end
