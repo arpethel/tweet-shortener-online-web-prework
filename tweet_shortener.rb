9f1187d0a4f3b6da71309260d280c8b55fef4c1c
Ryan Pethel <arpethel@gmail.com>
2019-02-28 21:44:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,8 +29,8 @@ end
 
 def bulk_tweet_shortener(tweets)
   str = tweets.join
-  result = word_substituter(str)
-  puts result
+  puts word_substituter(str)
+  # puts result
   # word_substituter(tweets)
 end
 
