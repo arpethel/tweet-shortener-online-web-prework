c2942dbc4eb82e6a858c489f944f59d73ea0de3a
Ryan Pethel <arpethel@gmail.com>
2019-02-28 18:59:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def dictionary
+  words = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => '@',
+    "and" => "&"
+  }
+end
+def word_substituter(tweet)
+
+  tweet_arr = tweet.split
+  tweet_arr.each do |word|
+    dictionary_keys = dictionary.keys
+    if dictionary_keys.include?(word)
+      puts word
+      # tweet_arr.replace.values_at(word)
+    end
+  end
+  # result = tweet_arr.join
+  # return result
+end
